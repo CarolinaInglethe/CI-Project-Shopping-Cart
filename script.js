19b68d2dca7bc73f0c2db84e6ecd2fb3e357de8d
Carol_Inglethe <carolinainglethe@gmail.com>
2021-07-08 19:30:13 -0300

    [Carolina-Inglethe]Project-shoping-cart

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+const elementItems = document.querySelector('.items');
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
   img.src = imageSource;
   return img;
-}
+} 
 
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
@@ -20,24 +22,42 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
   return section;
 }
 
-function getSkuFromProductItem(item) {
+/* function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
-}
+} */
 
-function cartItemClickListener(event) {
+/* function cartItemClickListener(event) {
   // coloque seu código aqui
-}
+} */
 
-function createCartItemElement({ sku, name, salePrice }) {
+/* function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
+} */
+
+// objeto atraves do meu request envio para criar novo elemento:
+function myObjComputer(computer) {
+  const { id: sku, title: name, thumbnail: image, price: salePrice } = computer;
+  console.log(computer);
+  return elementItems.appendChild(createProductItemElement({ sku, name, image, salePrice }));
 }
 
-window.onload = () => { };
+// fazendo o request :
+function getAPI(url) {
+  return fetch(url)
+  .then((result) => result.json())
+  .then((object) => object.results)
+  .then((results) => results.forEach((computer) => {
+    myObjComputer(computer);
+    }));
+}
+
+window.onload = () => {
+  getAPI('https://api.mercadolibre.com/sites/MLB/search?q=$computador');
+};
